ad1a549e1908f51415781c590ce3eb23ad0d928a
Pops <pffijt@gmail.com>
2020-11-03 18:02:51 +0100

    start interpreter

---
+++ src/interpreter/can_open_interpreter.py
@@ -0,0 +1,10 @@
+from interpreter.interpreter import Interpreter
+
+
+class CanOpenInterpreter(Interpreter):
+    def __init__(self):
+        super().__init__()
+
+    def inform_interpreter(self, sdo_object):
+        # Work in progress
+        print(sdo_object)

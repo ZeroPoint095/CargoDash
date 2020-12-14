d64378307376c2d6c6cada4faec046aeac04a34f
Pops <pffijt@gmail.com>
2020-12-14 13:47:23 +0100

    Made the code somewhat cleaner

--- src/main.py
+++ src/main.py
@@ -15,17 +15,18 @@ if __name__ == "__main__":
     config_type = 'canopen_vcan'
     with open('./config.yaml', 'r') as ymlfile:
         config = yaml.safe_load(ymlfile)
-    TestNetwork = CanOpenTestNetwork(config, config_type)
+    if(config[config_type]['communication_protocol'] == 'canopen'):
+        TestNetwork = CanOpenTestNetwork(config, config_type)
 
-    if(TestNetwork.connected_to_network()):
-        TestNetwork.test_connection()
+        if(TestNetwork.connected_to_network()):
+            TestNetwork.test_connection()
 
-        coach = ConfigureableVehicle(config, config_type)
-        can_open_interpreter = CanOpenInterpreter(coach)
-        master_node = CanOpenListener(config, config_type,
-                                      can_open_interpreter)
-        logger = CanBufferLogger(config, config_type)
+            coach = ConfigureableVehicle(config, config_type)
+            can_open_interpreter = CanOpenInterpreter(coach)
+            master_node = CanOpenListener(config, config_type,
+                                          can_open_interpreter)
+            logger = CanBufferLogger(config, config_type)
 
-        # Runs master-node and logger concurrently.
-        asyncio.run(concurrently(master_node.async_network_loop(),
-                                 logger.listen_to_network()))
+            # Runs master-node and logger concurrently.
+            asyncio.run(concurrently(master_node.async_network_loop(),
+                                     logger.listen_to_network()))

0684cdd131d4a054f9235a309c82058a8de4609f
Pops <pffijt@gmail.com>
2021-01-07 12:55:42 +0100

    addded startup scripts

---
+++ startup-test-environment.sh
@@ -0,0 +1,7 @@
+sudo ip link add dev vcan0 type vcan
+sudo ip link set up vcan0
+
+gnome-terminal --working-directory=$(pwd)/src/slave_node/ --command="python3 measuring_node.py"
+gnome-terminal --working-directory=$(pwd)/src/ --command="python3 main.py"
+gnome-terminal --working-directory=$(pwd)/src/http_server/ --command="python3 cargo_dash_http_server.py"
+gnome-terminal --working-directory=$(pwd)/src/frontend --command="python3 -m http.server"

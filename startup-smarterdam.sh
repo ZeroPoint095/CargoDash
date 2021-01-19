f74e66d03d66c1f5679df588435c53f14b08828a
Pops <pffijt@gmail.com>
2021-01-19 16:08:33 +0100

    Added startup script for smarterdam vehicle

--- startup-smarterdam.sh
+++ startup-smarterdam.sh
@@ -1,1 +1,7 @@
 # It's empty for now we can fix this at the end.
+sudo ip link set can0 up type can bitrate 500000
+sudo ifconfig can0 txqueuelen 10000
+
+lxterminal --working-directory=$(pwd)/src/ --command="python3 main.py"
+lxterminal --working-directory=$(pwd)/src/http_server/ --command="python3 cargo_dash_http_server.py"
+lxterminal --working-directory=$(pwd)/src/frontend --command="python3 -m http.server"
